162f79692a48e07176581790ba0a284c5fd2c7cd
Samuel Chua <chua.jiacong.samuel@dhs.sg>
2023-04-22 19:48:49 -0700

    Added Firebase Linkage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { initializeAuth, getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPtXjocTf0p45AJmVv-HVuW8ap9W5li-M",
+  authDomain: "clockwise-b4b26.firebaseapp.com",
+  projectId: "clockwise-b4b26",
+  storageBucket: "clockwise-b4b26.appspot.com",
+  messagingSenderId: "537356577294",
+  appId: "1:537356577294:web:be109758f24d9569b26a5f",
+  measurementId: "G-9V4BMV59XX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, user => {
+  if (user != null) {
+    console.log("logged in")
+    // setCurrentUser(user);
+  } else {
+    console.log("no user")
+    // setCurrentUser(null);
+  }
+}, (error) => {
+  console.error('Error during authentication state change: ', error);
+});
+
+
+// Authentication of Users 
+// Location/Time/Class
+// List of students and status 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +47,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
